41f8f2f6e8707793b42c7e8971716419656f012d
vicheath <vicheath.a.s@gmail.com>
2023-11-21 23:03:23 -0500

    finishpractice

--- index.js
+++ index.js
@@ -29,23 +29,27 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 console.log(isTwoOdd)
 
 
-
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isOver25 = n1 <= 25 || n2 <= 25 || n3 <= 25 || n4 <= 25;
+
+console.log(isOver25)
+
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log(isUnique)
+
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isOver25 && isUnique;
 
 // Finally, log the results.
 console.log(isValid);
